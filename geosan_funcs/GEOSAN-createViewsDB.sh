351cc3f659d2b23ebbe38759fdc951fe4a27d224
aladoy <anais.ladoy@epfl.ch>
2022-09-13 18:44:07 +0200

    create views for vd ha grid

--- geosan_funcs/GEOSAN-createViewsDB.sh
+++ geosan_funcs/GEOSAN-createViewsDB.sh
@@ -16,6 +16,21 @@ AS
 SELECT * FROM municipalities WHERE canton_num=22;
 
 
+CREATE MATERIALIZED VIEW IF NOT EXISTS vd_reli_point
+AS
+SELECT r.* FROM reli_point r, cantons c WHERE c.name='Vaud' AND st_intersects(r.geometry, c.geometry);
+
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS vd_reli_centroid
+AS
+SELECT r.* FROM reli_centroid r, vd_reli_point v WHERE v.reli=r.reli;
+
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS vd_reli_polygon
+AS
+SELECT r.* FROM reli_polygon r, vd_reli_point v WHERE v.reli=r.reli;
+
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS palm_municipalities
 AS
 SELECT * FROM municipalities WHERE name IN ('Belmont-sur-Lausanne', 'Lutry', 'Paudex', 'Pully', 'Boussens', 'Bretigny-sur-Morrens', 'Cheseaux-sur-Lausanne', 'Cugy', 'Froideville', 'Jouxtens-Mézery', 'Lausanne', 'Epalinges', 'Le Mont-sur-Lausanne', 'Morrens', 'Prilly', 'Romanel-sur-Lausanne', 'Sullens', 'Bussigny', 'Chavannes-près-Renens', 'Crissier', 'Ecublens', 'Prilly', 'Renens (VD)', 'St-Sulpice', 'Villars-Ste-Croix');
