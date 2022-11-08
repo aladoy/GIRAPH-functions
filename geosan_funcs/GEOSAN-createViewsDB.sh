3f639412bdf203958be7374b8c3b893f46e0047d
aladoy <anais.ladoy@epfl.ch>
2022-11-08 16:27:00 +0100

    fix vd_mun

--- geosan_funcs/GEOSAN-createViewsDB.sh
+++ geosan_funcs/GEOSAN-createViewsDB.sh
@@ -7,41 +7,40 @@ HOSTNAME='localhost'
 psql -h $HOSTNAME -U $USERNAME $DATABASE << EOF
 
 DROP MATERIALIZED VIEW IF EXISTS vd_canton;
+DROP MATERIALIZED VIEW IF EXISTS vd_mun;
+DROP MATERIALIZED VIEW IF EXISTS vd_reli_point;
+DROP MATERIALIZED VIEW IF EXISTS vd_reli_centroid;
+DROP MATERIALIZED VIEW IF EXISTS vd_reli_polygon;
+DROP MATERIALIZED VIEW IF EXISTS lausanne_region_mun;
+DROP MATERIALIZED VIEW IF EXISTS vd_mgis_ha;
+DROP MATERIALIZED VIEW IF EXISTS vd_mgis_mun;
+DROP MATERIALIZED VIEW IF EXISTS lausanne_reli_polygon;
+
 
 CREATE MATERIALIZED VIEW vd_canton
 AS
 SELECT * FROM cantons WHERE name='Vaud';
 
 
-DROP MATERIALIZED VIEW IF EXISTS vd_mun;
-
 CREATE MATERIALIZED VIEW vd_mun
 AS
-SELECT * FROM municipalities WHERE kantonsnum='CH22270000';
+SELECT * FROM municipalities WHERE kantonsnum='CH22000000';
 
-
-DROP MATERIALIZED VIEW IF EXISTS vd_reli_point;
 
 CREATE MATERIALIZED VIEW vd_reli_point
 AS
 SELECT r.* FROM reli_point r, cantons c WHERE c.name='Vaud' AND st_intersects(r.geometry, c.geometry);
 
 
-DROP MATERIALIZED VIEW IF EXISTS vd_reli_centroid;
-
 CREATE MATERIALIZED VIEW vd_reli_centroid
 AS
 SELECT r.* FROM reli_centroid r, vd_reli_point v WHERE v.reli=r.reli;
 
 
-DROP MATERIALIZED VIEW IF EXISTS vd_reli_polygon;
-
 CREATE MATERIALIZED VIEW vd_reli_polygon
 AS
 SELECT r.* FROM reli_polygon r, vd_reli_point v WHERE v.reli=r.reli;
 
-
-DROP MATERIALIZED VIEW IF EXISTS lausanne_region_mun;
 
 CREATE MATERIALIZED VIEW lausanne_region_mun
 AS
@@ -49,18 +48,21 @@ SELECT * FROM municipalities WHERE name IN ('Belmont-sur-Lausanne', 'Lutry', 'Jo
 AND icc='CH';
 
 
-DROP MATERIALIZED VIEW IF EXISTS vd_mgis_ha;
-
 CREATE MATERIALIZED VIEW vd_mgis_ha
 AS
 SELECT * FROM microgis_ha WHERE kt_21=22;
 
 
-DROP MATERIALIZED VIEW IF EXISTS vd_mgis_mun;
-
 CREATE MATERIALIZED VIEW vd_mgis_mun
 AS
-SELECT * FROM microgis_mun where ktacro='VD'
+SELECT * FROM microgis_mun where ktacro='VD';
+
+
+CREATE MATERIALIZED VIEW lausanne_reli_polygon
+AS
+SELECT r.reli, r.geometry FROM vd_reli_polygon r, 
+(SELECT ST_Union(geometry) as geometry FROM lausanne_sectors) laus 
+WHERE ST_Intersects(r.geometry, laus.geometry);
 
 
 EOF
