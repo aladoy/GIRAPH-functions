ede2be490aa3b2cabeb82bd514b9376c80ea573a
aladoy <anais.ladoy@epfl.ch>
2023-02-09 14:30:17 +0100

    improve find_nearest_reli() to use polygon geom for hectares instead of points

--- basic_utils.py
+++ basic_utils.py
@@ -1,9 +1,11 @@
 # FUNCTION TO SAVE FILES
 
+import os
+import geopandas as gpd
+from shapely import wkb
+import numpy as np
 
 def save_gdf(path, file_name, gdf, driver="GPKG", layer=None, del_exist=True):
-
-    import os
 
     file_src = os.sep.join([path, file_name])
 
@@ -18,44 +20,38 @@ def save_gdf(path, file_name, gdf, driver="GPKG", layer=None, del_exist=True):
         print("Error while saving data on disk")
 
 
-def find_nearest_reli(row, reli_centroids, radius=None):
+def find_nearest_reli(row, ha_poly, radius=None):
     """
     This function retrieves the nearest neighbor (Point) from a given Point.
     To speed-up the process, the user can restrict the set of destination
     points to a given neighborhood (radius around the point of origin).
     Outputs: nearest RELI index (NaN for isolated point)
     """
-    from shapely.ops import nearest_points
-    import numpy as np
+    try:
+        nearest_reli = ha_poly[ha_poly.geometry.contains(
+            row.geometry)]['reli'].values[0]
 
-    if radius is not None:
-        neighborhood = reli_centroids.loc[
-            reli_centroids.intersects(row.geometry.buffer(radius))
-        ]
+    except IndexError:
 
-        # if no RELI can be found around the given radius (isolated point)
-        if neighborhood.empty:
-            nearest_reli = np.nan
-        else:
-            nearest_reli = reli_centroids.loc[
-                reli_centroids.geometry
-                == nearest_points(row.geometry, neighborhood.geometry.unary_union)[1],
-                "reli",
-            ].values[0]
+        try:
 
-    else:
-        nearest_reli = reli_centroids.loc[
-            reli_centroids.geometry
-            == nearest_points(row.geometry, reli_centroids.geometry.unary_union)[1],
-            "reli",
-        ].values[0]
+            if radius is not None:
+
+                ha_poly = ha_poly.loc[
+                    ha_poly.intersects(row.geometry.buffer(radius))
+                ]
+
+            ha_poly['distance'] = ha_poly.geometry.distance(row.geometry)
+            ha_poly = ha_poly.sort_values(by='distance')
+            nearest_reli = ha_poly.iloc[0]['reli']
+
+        except IndexError:
+            nearest_reli = np.nan  # no hectares within this distance
 
     return nearest_reli
 
 
 def convert_3D_to_2D(gdf):
-    import geopandas as gpd
-    from shapely import wkb
 
     _drop_z = lambda geom: wkb.loads(wkb.dumps(geom, output_dimension=2))
     gdf.geometry = gdf.geometry.transform(_drop_z)
